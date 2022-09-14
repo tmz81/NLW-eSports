850dcff040cdf3e94843d4a3c9727132b90a56ab
tmz81 <tmz_81@hotmail.com>
2022-09-14 07:18:02 -0300

    🚀

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,16 @@
+import logoImg from "./assets/Logo.png";
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+
+      <h1 className="
+        text-white 
+        font-black 
+        text-6xl
+        mt-20
+      ">Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.</h1>
+    </div>
   )
 }
 
