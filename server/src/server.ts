7784feb1b6fab4e30e18a372a1e758b3f2607972
tmz81 <tmz_81@hotmail.com>
2022-09-22 15:23:47 -0300

    🚀

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,20 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
-  return response.json([
-    { id: 1, name: "Anúncio 1" },
-    { id: 2, name: "Anúncio 2" },
-    { id: 3, name: "Anúncio 3" },
-  ]);
+app.get("/games", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([])
+})
+
+app.get("/games/:id/ads  ", (request, response) => {
+  return response.json([]);
+});
+
+app.get("/ads/:id/discord  ", (request, response) => {
+  return response.json([]);
 });
 
 app.listen(3333);
