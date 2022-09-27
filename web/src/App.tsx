50942215b384a82bd9dfc127a3e9ac1b79e6bb93
tmz81 <tmz_81@hotmail.com>
2022-09-27 09:34:30 -0300

    🚀

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,26 @@
 import logoImg from "./assets/logo-nlw.svg";
 import { GameBanner } from "./components/GameBanner";
 import { CreateAdBanner } from "./components/CreateAdBanner";
+import { useEffect, useState } from "react";
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [])
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
@@ -12,7 +30,15 @@ function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner bannerUlr="/image3.png" title="Counter-Strike: Global Offensive" adsCount={1}/>
+        {games.map(game => {
+          return (
+            <GameBanner 
+              bannerUlr={game.bannerUrl} 
+              title={game.title} 
+              adsCount={game._count.ads}
+            />
+          )
+        })}
       </div> 
 
       <CreateAdBanner />
